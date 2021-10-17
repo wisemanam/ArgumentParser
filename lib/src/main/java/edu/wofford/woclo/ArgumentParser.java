f0a1248d606b8b738d281f08c885f6e9f5b31a42
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-10-17 19:27:13 -0400

    changed needsHelp to return a boolean value

--- lib/src/main/java/edu/wofford/woclo/ArgumentParser.java
+++ lib/src/main/java/edu/wofford/woclo/ArgumentParser.java
@@ -10,6 +10,7 @@ public class ArgumentParser {
 
   /**
    * ArgumentParser takes an integer and a string and parses the arguments for the user to retreive.
+   *
    * @param expectedArgs
    * @param arguments
    */
@@ -28,6 +29,7 @@ public class ArgumentParser {
 
   /**
    * Takes an integer and returns the corresponding string.
+   *
    * @param index
    * @return string corresponding to the index
    */
@@ -37,25 +39,21 @@ public class ArgumentParser {
 
   /**
    * Returns the number of words in the string passed to ArgumentParser.
+   *
    * @return the number of words in the string
    */
   public int numArgs() {
     return args_list.length;
   }
 
-  /**
-   * @return true if the number of words entered does not match the number of words expected
-   */
+  /** @return true if the number of words entered does not match the number of words expected */
   public boolean numWordsNotExpected() {
     return args_list.length != expectedArgs;
   }
 
   // Feature 2 Implementation
 
-  public void needsHelp() {
-    if (help) {
-      // something that tells program to stop and print help message
-    }
+  public boolean needsHelp() {
+    return help;
   }
-
 }
