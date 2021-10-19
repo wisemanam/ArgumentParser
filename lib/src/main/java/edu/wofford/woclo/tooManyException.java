1a40df73c4f062bc0eca05a9aa3165a43f40249f
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-10-18 21:49:48 -0400

    add javadocs for TooManyException

--- lib/src/main/java/edu/wofford/woclo/tooManyException.java
+++ lib/src/main/java/edu/wofford/woclo/tooManyException.java
@@ -1,14 +1,30 @@
 package edu.wofford.woclo;
 
+/**
+ * The TooManyException is thrown when the number of arguments given to ArgumentParser is greater
+ * than the number expected. The exception requires the expected number of arguments and the list of
+ * arguments given. It includes methods that return the first additional argument that was given to
+ * ArgumentParser.
+ */
 public class TooManyException extends RuntimeException {
   int expected;
   String[] args_list;
 
+  /**
+   * The constructor for the exception thrown when the number of arguments is greater than expected.
+   *
+   * @param expected the expected number of arguments given from the command line
+   * @param args_list the list of arguments from the command line
+   */
   public TooManyException(int expected, String[] args_list) {
     this.expected = expected;
     this.args_list = args_list.clone();
   }
-
+  /**
+   * Gets the first argument after the initial expected arguments.
+   *
+   * @return the first additional argument that is given on the command line
+   */
   public String getFirstExtra() {
     return args_list[expected];
   }
