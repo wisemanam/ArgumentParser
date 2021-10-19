a9ac50bf90e1c9181498065d5f48f701bd1b4193
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-10-18 21:57:44 -0400

    added javadocs

--- lib/src/main/java/edu/wofford/woclo/HelpException.java
+++ lib/src/main/java/edu/wofford/woclo/HelpException.java
@@ -1,10 +1,9 @@
 package edu.wofford.woclo;
-/**
- * The HelpException is thrown by ArgumentParser when "--help" or "--h" is one of the arguments.
- */
+/** The HelpException is thrown by ArgumentParser when "--help" or "--h" is one of the arguments. */
 public class HelpException extends RuntimeException {
   /**
    * Takes a message and prints it when HelpException is thrown.
+   *
    * @param message the message that the user wishes to print when the HelpException is thrown.
    */
   public HelpException(String message) {
--- lib/src/main/java/edu/wofford/woclo/tooFewException.java
+++ lib/src/main/java/edu/wofford/woclo/tooFewException.java
@@ -1,14 +1,27 @@
 package edu.wofford.woclo;
-
+/**
+ * The TooManyException is thrown when the number of arguments given to ArgumentParser is less
+ * than the number expected. The exception requires the expected number of arguments and the list of
+ * arguments given. It includes methods that return the name of the next expected argument.
+ */
 public class TooFewException extends RuntimeException {
   int expected;
   String[] args_list;
 
+  /**
+   * The constructor for the exception thrown when the number of arguments is less than expected.
+   *
+   * @param expected the expected number of arguments given from the command line
+   * @param args_list the list of arguments from the command line
+   */
   public TooFewException(int expected, String[] args_list) {
     this.expected = expected;
     this.args_list = args_list.clone();
   }
-
+  /**
+   * Gets the name of the next argument that ArgumentParser expects.
+   * @return the name of next argument
+   */
   public String getNextExpected() {
     return "string" + String.valueOf(args_list.length + 1);
   }
