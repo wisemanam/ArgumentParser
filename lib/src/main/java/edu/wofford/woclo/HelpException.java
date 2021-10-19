987b339cc490f242df86578f9537066007cb2e77
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-10-18 21:37:24 -0400

    added javadoc for HelpException

--- lib/src/main/java/edu/wofford/woclo/HelpException.java
+++ lib/src/main/java/edu/wofford/woclo/HelpException.java
@@ -1,6 +1,12 @@
 package edu.wofford.woclo;
-
+/**
+ * The HelpException is thrown by ArgumentParser when "--help" or "--h" is one of the arguments.
+ */
 public class HelpException extends RuntimeException {
+  /**
+   * Takes a message and prints it when HelpException is thrown.
+   * @param message the message that the user wishes to print when the HelpException is thrown.
+   */
   public HelpException(String message) {
     super(message);
   }
