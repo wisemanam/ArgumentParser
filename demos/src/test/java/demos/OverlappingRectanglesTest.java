82013e994b1de3639fd83662bc64695059f2f87f
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-10-27 18:28:46 -0400

    added second test case for OverlappingRectangles

--- demos/src/test/java/demos/OverlappingRectanglesTest.java
+++ demos/src/test/java/demos/OverlappingRectanglesTest.java
@@ -7,10 +7,18 @@ import org.junit.jupiter.api.*;
 public class OverlappingRectanglesTest {
 
   @Test
-  public void Test1() {
+  public void testOverlappingRectangles1() {
     String[] args = {"-3", "0", "3", "4", "0", "-1", "9", "2"};
     OverlappingRectangles o = new OverlappingRectangles();
     String s = o.overlappingRectangles(args);
     assertEquals(s, "6 45");
+  }
+
+  @Test
+  public void testOverlappingRectangles2() {
+    String[] args = {"-6", "2", "6", "6", "-5", "-5", "-2", "3"};
+    OverlappingRectangles o = new OverlappingRectangles();
+    String s = o.overlappingRectangles(args);
+    assertEquals(s, "3 69");
   }
 }
