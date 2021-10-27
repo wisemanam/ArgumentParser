c2e201ee600d5dcc1694dc3283c6e392037a8aad
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-10-27 16:50:56 -0400

    add a test case

--- demos/src/test/java/demos/OverlappingRectanglesTest.java
+++ demos/src/test/java/demos/OverlappingRectanglesTest.java
@@ -5,5 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 public class OverlappingRectanglesTest {
-
+    String[] args = {"-3", "0", "3", "4", "0", "-1", "9", "2"};
+    OverlappingRectangles o = new OverlappingRectangles();
+    String oString = overlapRec.overlappingRectangles(args);
+    assertEqual(oString, "6 45");
 }
