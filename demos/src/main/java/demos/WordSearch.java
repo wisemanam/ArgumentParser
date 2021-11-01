3e745d2ef9fa821d9c742ef738774a8df17b9c40
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-11-01 14:22:25 -0400

    started Word Search

--- demos/src/main/java/demos/WordSearch.java
+++ demos/src/main/java/demos/WordSearch.java
@@ -4,9 +4,19 @@ import edu.wofford.woclo.*;
 
 public class WordSearch {
 
+  public boolean search(args){
+    ArgumentParser argParse = new ArgumentParser(args, 4);
+    argParse.addPositional("grid", "string");
+    argParse.addPositional("target", "string");
+  }
   // need expected args (grid, word, [optional]: width, height)
   // String[] expectedArgs = {grid, word, width, height};
   // if arguments doesnt contain --width or --height
   // do setdefault for whatever it is missing
-  public static void main(String... args) {}
+
+  public static void main(String... args) {
+    WordSearch w = new WordSearch();
+    boolean b = w.search(args);
+    System.out.println(b);
+  }
 }
