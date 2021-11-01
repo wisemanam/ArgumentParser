eec84cdf0f00e5e389af49845a36fa99000b4ff0
Emily Moore <emilymoore@Emilys-MacBook-Air-4.local>
2021-10-31 21:19:56 -0400

    start feature 4 and 5

--- demos/src/main/java/demos/OverlappingRectangles.java
+++ demos/src/main/java/demos/OverlappingRectangles.java
@@ -9,6 +9,7 @@ public class OverlappingRectangles {
     String[] expectedArgs = {"x1", "y1", "x2", "y2", "x3", "y3", "x4", "y4"};
     try {
       ArgumentParser argParse = new ArgumentParser(8, arguments);
+      argParse.setExpectedArgNames(expectedArgs);
       int x1rec1 = argParse.getInt(0);
       int y1rec1 = argParse.getInt(1);
       int x2rec1 = argParse.getInt(2);
--- lib/src/main/java/edu/wofford/woclo/ArgumentParser.java
+++ lib/src/main/java/edu/wofford/woclo/ArgumentParser.java
@@ -10,7 +10,8 @@ import java.util.*;
  * will not change the contents of the ArgumentParser.
  */
 public class ArgumentParser {
-  private String[] args_list;
+  private HashMap<String, String> args;
+  private String[] expected_names;
 
   /**
    * ArgumentParser takes an integer and a string and parses the arguments for the user to retreive.
@@ -21,15 +22,48 @@ public class ArgumentParser {
    * @param arguments a list of the arguments the client would like to parse
    */
   public ArgumentParser(int expected_args, String[] arguments) {
-    args_list = arguments.clone();
+    args = new HashMap<String, String>();
     int expectedArgs = expected_args;
-    if (Arrays.asList(args_list).contains("--help") || Arrays.asList(args_list).contains("-h")) {
+    if (Arrays.asList(arguments).contains("--help") || Arrays.asList(arguments).contains("-h")) {
       throw new HelpException("Help needed.");
-    } else if (expectedArgs > args_list.length) {
-      throw new TooFewException(expectedArgs, args_list);
-    } else if (expectedArgs < args_list.length) {
-      throw new TooManyException(expectedArgs, args_list);
+    } else if (expectedArgs > arguments.length) {
+      throw new TooFewException(expectedArgs, arguments);
+    } else if (expectedArgs < arguments.length) {
+      throw new TooManyException(expectedArgs, arguments);
     }
+    int i = 0;
+    int numDefaults = 1;
+    while (i < arguments.length) {
+      if (arguments[i].contains("--")) {
+        String name = arguments[i].substring(2, arguments[i].length());
+        args.put(name, arguments[i + 1]);
+        i = i + 2;
+      } else {
+        String name = "__default" + Integer.toString(numDefaults);
+        args.put(name, arguments[i]);
+        i++;
+        numDefaults++;
+      }
+    }
+  }
+  
+  /**
+   * Takes an array of strings with the names of the expected arguments.
+   *
+   * @param expected_arg_names array of strings that contains the names of expected arguments.
+   */
+  public void setExpectedArgNames(String[] expected_arg_names) {
+    this.expected_names = expected_names;
+  }
+
+  /**
+   * Takes an integer representing an index value and returns the name of the expected argument at that index
+   *
+   * @param index index of the string to be returned.
+   * @return string corresponding to the index
+   */
+  public String getExpectedArgValue(int index) {
+    return expected_names[index];
   }
 
   /**
@@ -38,8 +72,19 @@ public class ArgumentParser {
    * @param index index of the string to be returned
    * @return string corresponding to the index
    */
-  public String getString(int index) {
-    return args_list[index];
+  public String getString(int position) {
+    String name = "__default" + Integer.toString(position);
+    return args.get(name);
+  }
+
+  // this is for when they give us the name of a non-positional argument
+  public String getString(String arg_name) {
+    try {
+      String argument = args.get(arg_name);
+      return argument;
+    } catch (NumberFormatException e) {
+      throw new WrongTypeException(args.get(arg_name));
+    }
   }
 
   /**
@@ -49,14 +94,26 @@ public class ArgumentParser {
    * @param index index of the integer to be returned
    * @return integer corresponding to the index
    */
-  public int getInt(int index) {
+  public int getInt(int position) {
+    String name = "__default" + Integer.toString(position);
     try {
-      int argument = Integer.parseInt(args_list[index]);
+      int argument = Integer.parseInt(args.get(name));
       return argument;
     } catch (NumberFormatException e) {
-      throw new WrongTypeException(args_list[index]);
+      throw new WrongTypeException(args.get(name));
     }
   }
+
+  // this is for when they give us the name of a non-positional argument
+  public int getInt(String arg_name) {
+    try {
+      int argument = Integer.parseInt(args.get(arg_name));
+      return argument;
+    } catch (NumberFormatException e) {
+      throw new WrongTypeException(args.get(arg_name));
+    }
+  }
+
 
   /**
    * Takes an integer and returns the corresponding float value. If the value at the index give
@@ -65,12 +122,24 @@ public class ArgumentParser {
    * @param index index of the float to be returned
    * @return float corresponding to the index
    */
-  public float getFloat(int index) {
+  public float getFloat(int position) {
+    String name = "__default" + Integer.toString(position);
     try {
-      float argument = Float.parseFloat(args_list[index]);
+      float argument = Float.parseFloat(args.get(name));
       return argument;
     } catch (NumberFormatException e) {
-      throw new WrongTypeException(args_list[index]);
+      throw new WrongTypeException(args.get(name));
+    }
+  }
+
+
+  // this is for when they give us the name of a non-positional argument
+  public float getFloat(String arg_name) {
+    try {
+      float argument = Float.parseFloat(args.get(arg_name));
+      return argument;
+    } catch (NumberFormatException e) {
+      throw new WrongTypeException(args.get(arg_name));
     }
   }
 
@@ -80,6 +149,6 @@ public class ArgumentParser {
    * @return the number of words in the string
    */
   public int numArgs() {
-    return args_list.length;
+    return args.size();
   }
 }
