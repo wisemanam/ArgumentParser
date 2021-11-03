7f6d6f339dd401ff9eaeff3a2a04cb1753ba3988
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-11-03 13:17:54 -0400

    added word search pseudocode

--- demos/src/main/java/demos/WordSearch.java
+++ demos/src/main/java/demos/WordSearch.java
@@ -42,10 +42,6 @@ public class WordSearch {
     return true;
     // word search
   }
-  // need expected args (grid, word, [optional]: width, height)
-  // String[] expectedArgs = {grid, word, width, height};
-  // if arguments doesnt contain --width or --height
-  // do setdefault for whatever it is missing
 
   public static void main(String... args) {
     WordSearch w = new WordSearch();
