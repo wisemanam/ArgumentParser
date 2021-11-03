b8a09c47aafbd253373e8ed52dd3b22b6ec7c12a
Savannah Bryant <bryantsl@Savannahs-MacBook.local>
2021-11-03 17:51:08 -0400

    Word Search changes

--- demos/src/test/java/demos/WordSearchTest.java
+++ demos/src/test/java/demos/WordSearchTest.java
@@ -1,18 +1,32 @@
 package demos;
 
+import org.junit.jupiter.api.Test;
+
 public class WordSearchTest {
-  /*
-    @Test
-    public void testDefaultGrid() {
-        String grid = "h    a    l    ifax      ";
-        WordSearch wordsearch = new WordSearch();
-        assertTrue(wordsearch.search({grid, "halifax"}));
+  @Test
+  public void testDefaultGrid() {
+    String[] args = {"h    a    l    ifax      ", "halifax"};
+    WordSearch wordsearch = new WordSearch(args);
+    assertTrue(wordsearch.search());
 
-        String grid2 = "softsweskaolzilklqmtreyoy";
-        WordSearch wordsearch2 = new WordSearch();
-        assertTrue(wordsearch2.search({grid2, "yellow"}));
+    String[] args = {"softsweskaolzilklqmtreyoy", "yellow"};
+    WordSearch wordsearch2 = new WordSearch();
+    assertTrue(wordsearch2.search(grid2, "yellow"));
 
-        String grid3 = "";
+    String grid3 = "w    ol    l    ey       ";
+    WordSearch wordsearch3 = new WordSearch();
+    assertTrue(wordsearch.search(grid3, "yellow"));
   }
-  */
+
+  @Test
+  public void testOverwriteHeightNoRepeat() {
+    String grid = "tacsdfgelhijkam"
+    WordSearch wordsearch = new WordSearch();
+    assertTrue(wordsearch.search(grid, "malecat"));
+  }
+
+  @Test
+  public void testOverwriteWidthNoRepeat() {
+    String grid = ""
+  }
 }
