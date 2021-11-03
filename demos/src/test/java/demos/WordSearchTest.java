777e124b90619481a356abe540883943e45def55
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-11-03 17:00:23 -0400

    commented out Word Search Tests

--- demos/src/test/java/demos/WordSearchTest.java
+++ demos/src/test/java/demos/WordSearchTest.java
@@ -1,18 +1,18 @@
-package demos;
+// package demos;
 
-public class WordSearchTest {
-  /*
-    @Test
-    public void testDefaultGrid() {
-        String grid = "h    a    l    ifax      ";
-        WordSearch wordsearch = new WordSearch();
-        assertTrue(wordsearch.search({grid, "halifax"}));
+// public class WordSearchTest {
+//   /*
+//     @Test
+//     public void testDefaultGrid() {
+//         String grid = "h    a    l    ifax      ";
+//         WordSearch wordsearch = new WordSearch();
+//         assertTrue(wordsearch.search({grid, "halifax"}));
 
-        String grid2 = "softsweskaolzilklqmtreyoy";
-        WordSearch wordsearch2 = new WordSearch();
-        assertTrue(wordsearch2.search({grid2, "yellow"}));
+//         String grid2 = "softsweskaolzilklqmtreyoy";
+//         WordSearch wordsearch2 = new WordSearch();
+//         assertTrue(wordsearch2.search({grid2, "yellow"}));
 
-        String grid3 = "";
-  }
-  */
-}
+//         String grid3 = "";
+//   }
+//   */
+// }
