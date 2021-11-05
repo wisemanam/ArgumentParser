7002966026f696ad200ed0f4343c4b673c9f78b9
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-11-04 20:43:07 -0400

    added javadoc to Argument

--- lib/src/main/java/edu/wofford/woclo/Argument.java
+++ lib/src/main/java/edu/wofford/woclo/Argument.java
@@ -1,32 +1,45 @@
 package edu.wofford.woclo;
 // import java.lang.ProcessBuilder.Redirect.Type;
-
+/**
+ * Argument collects information about what is expected to come in on the command line. Each
+ * argument has a corresponding name, type, value, and description.
+ */
 public class Argument {
   protected String name;
   protected String type;
   protected String description;
   protected String value;
-
+  /**
+   * When an instance of Argument is created, it requires the arguments associated name, the type of
+   * the value, and the description used in the help message.
+   *
+   * @param name the name associated with the incoming value
+   * @param type the type of the value
+   * @param description the description used in the help message
+   */
   public Argument(String name, String type, String description) {
     this.name = name;
     this.type = type;
     this.description = description;
   }
-
+  /**
+   * setValue takes a string and sets the value equal to that string
+   *
+   * @param value
+   */
   public void setValue(String value) {
     this.value = value;
   }
-
+  /**
+   * returns the value of the argument in the type specified in the constructor
+   *
+   * @param <T>
+   * @return
+   */
   public <T> T getValue() {
-<<<<<<< HEAD
-    if (type == "string") {
-      return (T) value;
-    } else if (type == "integer") {
-=======
     if (type.equals("string")) {
       return (T) value;
     } else if (type.equals("integer")) {
->>>>>>> 797b7a432fbbd12c716fea2a185bc95b9ac2d17d
       try {
         return (T) Integer.valueOf(value);
       } catch (NumberFormatException e) {
@@ -40,15 +53,27 @@ public class Argument {
       }
     }
   }
-
+  /**
+   * Gets the type of the value in the form of a string
+   *
+   * @return the type of the value
+   */
   public String getType() {
     return type;
   }
-
+  /**
+   * Gets the name associated with the argument
+   *
+   * @return the name of the argument
+   */
   public String getName() {
     return name;
   }
-
+  /**
+   * Gets the description used in the help message
+   *
+   * @return description string
+   */
   public String getDescription() {
     return description;
   }
