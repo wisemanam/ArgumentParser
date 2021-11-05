530b314193b0eb13cb936c3970b470042833a303
Savannah Bryant <bryantsl@Savannahs-MacBook.local>
2021-11-04 20:42:25 -0400

    ArgumentNameNotSpecified javadoc

--- lib/src/main/java/edu/wofford/woclo/ArgumentNameNotSpecifiedException.java
+++ lib/src/main/java/edu/wofford/woclo/ArgumentNameNotSpecifiedException.java
@@ -1,12 +1,29 @@
 package edu.wofford.woclo;
 
+/**
+ * The ArgumentNameNotSpecifiedException is thrown when the name of an argument given to ArgumentParser 
+ * does not match an expected argument name. The exception requires the argument name given to the 
+ * ArgumentParser. It includes methods that return the name of the argument that does not match an 
+ * expected argument name.
+ */
 public class ArgumentNameNotSpecifiedException extends RuntimeException {
   String arg_name;
 
+  /**
+   * The constructor for the exception thrown when when the name of an argument given to 
+   * ArgumentParser does not match an expected argument name.
+   * 
+   * @param arg_name the name of the argument that does not match an expected argument name
+   */
   public ArgumentNameNotSpecifiedException(String arg_name) {
     this.arg_name = arg_name;
   }
 
+  /**
+   * Gets the name of the argument that does not match an expected argument name
+   * 
+   * @return the name of the argument that does not match an expected argument name
+   */
   public String getArgName() {
     return arg_name;
   }
