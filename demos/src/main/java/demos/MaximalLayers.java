a6c9f7e5868354662051e7ae9895a336bb19f807
Savannah Bryant <bryantsl@SavannasMacBook.wofford.int>
2021-11-09 19:54:47 -0500

    more layers

--- demos/src/main/java/demos/MaximalLayers.java
+++ demos/src/main/java/demos/MaximalLayers.java
@@ -1,13 +1,31 @@
 package demos;
 
 import edu.wofford.woclo.*;
+import java.util.*;
+import java.awt.*;
 
 public class MaximalLayers {
   public String maximialLayers(String[] arguments) {
     ArgumentParser parser = new ArgumentParser();
     try {
+      parser.addPositional("points", "string", "the data points");
       parser.addNonPositional("sortedX", "boolean", "sort layers by x coordinate", "false");
       parser.addNonPositional("sortedY", "boolean", "sort layers by y coordinate", "false");
+      parser.parse(arguments);
+      String points = parser.getValue("points");
+      Boolean sortedX = parser.getValue("sortedX");
+      Boolean sortedY = parser.getValue("sortedY");
+      String[] pointSplit = points.split(",", 0);
+      if (pointSplit.length % 2 == 1) return "MaximalLayers error: " + points.substring(points.length() - 1) + " is an unpaired x coordinate";
+      int[] point = new int[pointSplit.length];
+      for (int i = 0; i < pointSplit.length; i++) {
+        point[i] = Integer.parseInt(pointSplit[i]);
+      }
+      ArrayList<Point> pointList = new ArrayList<>();
+      for (int i = 0; i < point.length; i += 2) {
+        Point p = new Point(point[i], point[i + 1]);
+        pointList.add(p);
+      }
     } catch (HelpException e){
       return "help";
     } catch (WrongTypeException e) {
