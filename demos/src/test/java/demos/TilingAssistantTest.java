4185b260c5d46b09148d0b1ad7f2373b6de5de47
Savannah Bryant <bryantsl@SavannasMacBook.wofford.int>
2021-11-15 21:16:07 -0500

    first ft 7 test

--- demos/src/test/java/demos/TilingAssistantTest.java
+++ demos/src/test/java/demos/TilingAssistantTest.java
@@ -1,1 +1,13 @@
+package demos;
 
+import org.junit.jupiter.api.Test;
+
+public class TilingAssistantTest {
+    @Test
+    public void testBasicGrid() {
+        String[] args = {"4", "4", "--tilesize", "1"};
+        TilingAssistant tileassistant = new TilingAssistant();
+        String result = tileassistant.tileAssistant(args);
+        assertEquals(result, "9:(1.0 x 1.0 in)");
+    }
+}
