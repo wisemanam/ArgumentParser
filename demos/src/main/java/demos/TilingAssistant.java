8bc2295ef3b051673eb582c9320cf2f559f072c2
Savannah Bryant <bryantsl@SavannasMacBook.wofford.int>
2021-11-15 21:53:13 -0500

    tiling parsing

--- demos/src/main/java/demos/TilingAssistant.java
+++ demos/src/main/java/demos/TilingAssistant.java
@@ -3,6 +3,25 @@ package demos;
 import edu.wofford.woclo.*;
 
 public class TilingAssistant {
-
-  public static void main(String... args) {}
+  public String tilingAssistant(String[] arguments) {
+    ArgumentParser parser = new ArgumentParser();
+    try {
+      parser.addPositional("length", "float", "the length of the room");
+      parser.addPositional("width", "float", "the width of the room");
+      parser.addNonPositional("tilesize", "s", "float", "the size of the square tile", "6.0");
+      parser.addNonPositional("groutgap", "g", "float", "the width of the grout gap", "0.5");
+      parser.addNonPositional("metric", "m", "boolean", "use centimeters instead of inches", "false");
+      parser.addNonPositional("fullonly", "f", "boolean", "show only the full tiles required", "false");
+      parser.parse(arguments);
+      float length = parser.getValue("length");
+      float width = parser.getValue("width");
+      float tileSize = parser.getValue("tilesize");
+      float groutGap = parser.getValue("groutgap");
+      boolean metric = parser.getValue("metric");
+      boolean fullOnly = parser.getValue("fullonly");
+    }
+  }
+  public static void main(String... args) {
+    
+  }
 }
