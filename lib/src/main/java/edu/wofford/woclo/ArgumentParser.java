8057a8624d2aaa3e19791487306d2b76306b1dba
Emily Moore <emilymoore@Emilys-Air-4.wofford.int>
2021-11-17 12:45:45 -0500

    fixed number thing

--- lib/src/main/java/edu/wofford/woclo/ArgumentParser.java
+++ lib/src/main/java/edu/wofford/woclo/ArgumentParser.java
@@ -265,6 +265,8 @@ public class ArgumentParser {
                   }
                 }
               }
+            } else {
+              a.setValue("true");
             }
           }
         }
