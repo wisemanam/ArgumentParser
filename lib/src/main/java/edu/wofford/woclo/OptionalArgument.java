660582da7d9a2613d09d614a4a7a0f398868dfd8
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-11-18 16:07:08 -0500

    added docs to OptionalArgument

--- lib/src/main/java/edu/wofford/woclo/OptionalArgument.java
+++ lib/src/main/java/edu/wofford/woclo/OptionalArgument.java
@@ -18,6 +18,7 @@ public class OptionalArgument extends Argument {
    * constructed.
    *
    * @param name the name associated with the value
+   * @param short_name the short name associated with the value
    * @param type the type of the value
    * @param description a description of the argument used for the help message
    * @param value the value associated with the argument
@@ -30,6 +31,13 @@ public class OptionalArgument extends Argument {
     accepted_values = null;
   }
 
+  /**
+   * Takes the arguments name, type, description, and value, and creates an OptionalArgument.
+   * @param name the name associated with the value
+   * @param type the type of the value
+   * @param description a description of the argument used for the help message
+   * @param value the value associated with the argument
+   */
   public OptionalArgument(String name, String type, String description, String value) {
     super(name, type, description);
     this.value = value;
@@ -37,6 +45,16 @@ public class OptionalArgument extends Argument {
     accepted_values = null;
   }
 
+  /**
+   * Takes the arguments name, shorthand name, type, description, value, and list of accepted values and 
+   * creates an OptionalArgument.
+   * @param name the name associated with the value
+   * @param short_name the designated shorthand name of the argument
+   * @param type the type of the value
+   * @param description a description of the argument used for the help message
+   * @param value the value associated with the argument
+   * @param accepted_values a list of the argument's accepted values
+   */
   public OptionalArgument(
       String name,
       String short_name,
@@ -49,7 +67,15 @@ public class OptionalArgument extends Argument {
     this.short_name = short_name;
     this.accepted_values = accepted_values.clone();
   }
-
+  /**
+   * Takes the arguments name, type, description, value, and list of accepted values and creates 
+   * an OptionalArgument.
+   * @param name the name associated with the value
+   * @param type the type of the value
+   * @param description a description of the argument used for the help message
+   * @param value the value associated with the argument
+   * @param accepted_values list of the accepted values for the argument
+   */
   public OptionalArgument(
       String name, String type, String description, String value, String[] accepted_values) {
     super(name, type, description);
@@ -57,11 +83,13 @@ public class OptionalArgument extends Argument {
     short_name = "";
     this.accepted_values = accepted_values.clone();
   }
-  /** setValue sets the value of the Argument to be the value given as a parameter */
+  /** setValue sets the value of the Argument to be the value given as a parameter*/
   public void setValue(String value) {
     this.value = value;
   }
-  /** getValue returns the value of the argument in the type specified in the constructor */
+  /** getValue returns the value of the argument in the type specified in the constructor 
+   * @return the value of the argument in the specified type
+  */
   @Override
   public <T> T getValue() {
     if (type.equals("string")) {
@@ -82,7 +110,10 @@ public class OptionalArgument extends Argument {
       return (T) Boolean.valueOf(value);
     }
   }
-
+  /**
+   * Returns the short name of the argument
+   * @return the designated short name of the argument
+   */
   public String getShortName() {
     return short_name;
   }
