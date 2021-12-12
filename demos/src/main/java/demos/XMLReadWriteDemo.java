20267c710e637b40f09b12ea108c180affbd6c8d
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-12-12 13:25:41 -0500

    spotbugs in XMLReadWrite Demo

--- demos/src/main/java/demos/XMLReadWriteDemo.java
+++ demos/src/main/java/demos/XMLReadWriteDemo.java
@@ -1,17 +1,25 @@
 package demos;
 
 import edu.wofford.woclo.*;
+import java.io.*;
 
 public class XMLReadWriteDemo {
-  public static String readWrite(args) {
+  public static void readWrite(String[] args) throws IOException {
     String xmlString = args[0];
     String fileName = args[1];
     ArgumentParser argParse = XMLparser.parseXML(xmlString);
     String resultingXMLString = XMLparser.parserToXML(argParse);
-    return resultingXMLString;
+
+    FileWriter fw = new java.io.FileWriter(fileName);
+    fw.write(resultingXMLString);
+    fw.close();
   }
 
   public static void main(String... args) {
-    System.out.println(readWrite(args));
+    try {
+      readWrite(args);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
