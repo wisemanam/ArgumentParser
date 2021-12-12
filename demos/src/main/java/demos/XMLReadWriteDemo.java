64debfd035e41dfb9255a09ce50df364c3b759b8
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-12-12 13:10:13 -0500

    I think I got XMLReadWrite working

--- demos/src/main/java/demos/XMLReadWriteDemo.java
+++ demos/src/main/java/demos/XMLReadWriteDemo.java
@@ -3,6 +3,15 @@ package demos;
 import edu.wofford.woclo.*;
 
 public class XMLReadWriteDemo {
+  public static String readWrite(args) {
+    String xmlString = args[0];
+    String fileName = args[1];
+    ArgumentParser argParse = XMLparser.parseXML(xmlString);
+    String resultingXMLString = XMLparser.parserToXML(argParse);
+    return resultingXMLString;
+  }
 
-  public static void main(String... args) {}
+  public static void main(String... args) {
+    System.out.println(readWrite(args));
+  }
 }
