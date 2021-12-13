02d397ca6c94dd4db2df4d65e47d120cdf370003
Emily Moore <emilymoore@Emilys-Air-4.wofford.int>
2021-12-13 13:45:30 -0500

    xmlreadwritedemo

--- demos/src/main/java/demos/XMLReadWriteDemo.java
+++ demos/src/main/java/demos/XMLReadWriteDemo.java
@@ -6,11 +6,12 @@ import java.io.*;
 public class XMLReadWriteDemo {
   public static void readWrite(String[] args) throws IOException {
     OutputStreamWriter fw = null;
+    XMLparser x = new XMLparser();
     try {
       String xmlString = args[0];
       String fileName = args[1];
-      ArgumentParser argParse = XMLparser.parseXML(xmlString);
-      String resultingXMLString = XMLparser.parserToXML(argParse);
+      ArgumentParser argParse = x.parseXML(xmlString);
+      String resultingXMLString = x.parserToXML(argParse);
 
       FileOutputStream fileStream = new FileOutputStream(fileName);
       fw = new OutputStreamWriter(fileStream, "UTF-8");
