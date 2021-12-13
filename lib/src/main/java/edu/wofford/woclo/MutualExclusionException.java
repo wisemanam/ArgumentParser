8ac7e9f19f2cfd1c1edd39abebf741921e4da1ae
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-12-13 14:32:53 -0500

    added javadocs to MutualExclusionException

--- lib/src/main/java/edu/wofford/woclo/MutualExclusionException.java
+++ lib/src/main/java/edu/wofford/woclo/MutualExclusionException.java
@@ -2,9 +2,17 @@ package edu.wofford.woclo;
 
 import java.util.*;
 
+/**
+ * The MutualExclusionException is thrown when the user gives two or more arguments in the same mutually
+ * exclusive group.
+ */
 public class MutualExclusionException extends RuntimeException {
   private List<String> mutexc;
 
+  /**
+   * The constructor takes a group of mutually exclusive arguments.
+   * @param mutexc a group of mutually exclusive arguments
+   */
   public MutualExclusionException(List<String> mutexc) {
     List<String> cloned = new ArrayList<String>();
     for (int i = 0; i < mutexc.size(); i++) {
@@ -13,6 +21,10 @@ public class MutualExclusionException extends RuntimeException {
     this.mutexc = cloned;
   }
 
+  /**
+   * Returns the group of mutually exclusive arguments.
+   * @return the group of mutually exclusive arguments
+   */
   public List<String> getMutuallyExcList() {
     List<String> cloned = new ArrayList<String>();
     for (int i = 0; i < mutexc.size(); i++) {
