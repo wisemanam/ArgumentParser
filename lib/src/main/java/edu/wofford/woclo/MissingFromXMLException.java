f4f83f1775593f9f111c40ea2feab6d69ddc0408
Audrey Wiseman <wisemanam1@email.wofford.edu>
2021-12-13 14:19:31 -0500

    added javadocs for MissingFromXMLException

--- lib/src/main/java/edu/wofford/woclo/MissingFromXMLException.java
+++ lib/src/main/java/edu/wofford/woclo/MissingFromXMLException.java
@@ -1,12 +1,24 @@
 package edu.wofford.woclo;
 
+/**
+ * The MissingFromXMLException is thrown when there is something needed to construct the ArgumentParser that
+ * is not included in the XML string.
+ */
 public class MissingFromXMLException extends RuntimeException {
   private String missing;
 
+  /**
+   * The constructor takes the name of the missing argument.
+   * @param missing the name of the missing argument
+   */
   public MissingFromXMLException(String missing) {
     this.missing = missing;
   }
 
+  /**
+   * Returns the name of the missing argument.
+   * @return the name of thie missing argument.
+   */
   public String getMissing() {
     return missing;
   }
